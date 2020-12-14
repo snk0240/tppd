19b465cb2b7f6835b69785bb29c678d88d30f401
ricardo <ricardoroque98@gmail.com>
2020-12-14 02:52:16 +0000

    db

--- tppd/out/production/tppd/Server/server.bat
+++ tppd/out/production/tppd/Server/server.bat
@@ -1,10 +1,10 @@
 :: Server batch file
 
 cls
-set %1="5000"
-set %2="5001"
-set %3="localhost"
+set p1="5000"
+set p2="5001"
+set p3="localhost"
 del *.class ..\Dados\*.class
 javac *.java ..\Dados\*.java
-java ServerMain %1 %2 %3
+java ServerMain.java p1 p2 p3
 pause
